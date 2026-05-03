906758a464c0a5a78761c9cae6e8b2565c73c73e
harshitdas21-eng <harshitdas21@gmail.com>
2026-05-03 15:47:50 +0530

    env variables

--- imperial/src/Helper/UploadCloudinary.jsx
+++ imperial/src/Helper/UploadCloudinary.jsx
@@ -1,10 +1,10 @@
 export const uploadToCloudinary = async (file) => {
   const formData = new FormData();
   formData.append('file', file);
-  formData.append('upload_preset', import.meta.env.CLOUDINARY_PRESETNAME);
+  formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
 
   const res = await fetch(
-    `https://api.cloudinary.com/v1_1/${import.meta.env.CLOUDINARY_NAME}/image/upload`,
+    `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
     { method: 'POST', body: formData }
   );
 
